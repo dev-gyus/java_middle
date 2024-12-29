b3ab77231a1932c89487077af7a498620dc8ced6
GyuHyeong <cjsworbehd13@gmail.com>
2024-12-29 22:50:24 +0900

    1. 컬렉션 추가

---
+++ src/collection/array/MyArrayListV1Main.java
@@ -0,0 +1,17 @@
+package collection.array;
+
+public class MyArrayListV1Main {
+    public static void main(String[] args) {
+        MyArrayListV1 list = new MyArrayListV1();
+        System.out.println("==데이터 추가==");
+        System.out.println(list);
+        list.add("a");
+        System.out.println(list);
+        list.add("b");
+        System.out.println(list);
+        list.add("c");
+        System.out.println(list);
+
+        System.out.println("==기능 사용==");
+    }
+}
